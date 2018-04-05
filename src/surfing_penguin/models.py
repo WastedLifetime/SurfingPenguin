367659a02d9d995ddb5aab09d62f48d0953451af
zfrank7777 <b05902022@ntu.edu.tw>
2018-04-05 11:30:52 +0800

    Add password hash, not storing the real password

--- src/surfing_penguin/models.py
+++ src/surfing_penguin/models.py
@@ -3,24 +3,27 @@
 import datetime
 from sqlalchemy import Column, Integer, String, DateTime
 from surfing_penguin import Base
+from passlib.apps import custom_app_context as pwd_context
 
 
 class User(Base):
     __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
     username = Column(String(32))
-    password = Column(String(32))
+    password_hash = Column(String(256))
     register_time = Column(DateTime, default=datetime.datetime.utcnow)
     last_login = Column(DateTime, default=datetime.datetime.utcnow)
 
     def __init__(self, username, password):
         self.username = username
-        self.password = password
+        self.password = pwd_context.encrypt(password)
 
     def __repr__(self):
         return "User('{}','{}',registered at '{}',last login:'{}')".format(
                 self.username,
-                self.password,
                 self.register_time,
                 self.last_login
                 )
+
+    def verify_password(self, password):
+        return pwd_context.verify(password, self.password_hash)
