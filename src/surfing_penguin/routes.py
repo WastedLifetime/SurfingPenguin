811b947d74021c7b8d7efc6eba6bcc5254bdfa2d
zfrank7777 <b05902022@ntu.edu.tw>
2018-04-06 16:11:57 +0800

    Delete redundant codes and files

--- src/surfing_penguin/routes.py
+++ src/surfing_penguin/routes.py
@@ -1,10 +1,8 @@
 """routes.py: Each function in this file indicates a web page (HTML page)."""
 
-from surfing_penguin import surfing_penguin, api
-from flask import render_template, flash, redirect, url_for
+from surfing_penguin import api
 from flask_restplus import Resource, fields
-from surfing_penguin.forms import LoginForm
-from surfing_penguin.db_interface import Qstnr, UserFunctions
+from surfing_penguin.db_interface import UserFunctions
 
 question = api.model("question_model", {
         'content': fields.String,
@@ -30,34 +28,6 @@ api_show_user = api.model("show_user_model", {
         'username': fields.String,
         'id': fields.Integer,
     })
-
-
-qstnrs = [
-    {
-        'author': {'username': 'John'},
-        'title': 'QSTQ',
-        'id': 0
-    },
-    {
-        'author': {'username': 'Susan'},
-        'title': 'QSTQQ',
-        'id': 1
-    }
-]
-qstnr1 = Qstnr()
-qstnr1.new_qst({'content': 'Q1'})
-qstnr1.new_qst({'content': 'Q2'})
-
-
-@api.route('/api_display')
-class api_display(Resource):
-    """ This api is for developers to monitor the program,
-        currently only showing the questionnaire.
-        usage: start the server, and curl localhost:port/api_display """
-    @api.marshal_list_with(question)
-    def get(self):
-        return qstnr1.questions
-
 
 """ user account associated APIs:
     register, login, show_users, delete_user, search_user """
@@ -118,53 +88,6 @@ class search_user(Resource):
         if UserFunctions.search_user(search_name) is False:
             return {'messages': "user does not exist"}
         return UserFunctions.get_user(search_name)
-
-
-@api.route('/show_surveys')
-class show_surveys(Resource):
-    def get(self):
-        return qstnrs
-
-
-@api.route('/fill/<int:qstnr_id>')
-class fill(Resource):
-    def get(self, qstnr_id):
-        return qstnrs[qstnr_id]
-
-
-@surfing_penguin.route('/index')
-def index():
-    return render_template('index.html')
-
-
-@surfing_penguin.route('/login_html', methods=['GET', 'POST'])
-def login_html():
-    form = LoginForm()
-    if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(
-            form.username.data, form.remember_me.data))
-        return redirect(url_for('index'))
-    return render_template('login.html', title='Sign In', form=form)
-
-
-@surfing_penguin.route('/select_qstnr')
-def select_qstnr():
-    """Show the list of questionnaires.
-       Clients should pick up one and go to filling_in."""
-    return render_template('select_qstnr.html', qstnrs=qstnrs)
-
-
-@surfing_penguin.route('/filling_in')
-def filling_in():
-    qstnr = [
-        {
-            'question': 'test Q1'
-        },
-        {
-            'question': 'test Q2'
-        }
-    ]
-    return render_template('filling_in.html', qstnr=qstnr)
 
 
 def convert_user_to_json(user):
