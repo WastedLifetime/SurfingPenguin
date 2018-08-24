594c7bbce1fe0e6008f3d3694443b09f1289c0da
Allen <b03901014@ntu.edu.tw>
2018-08-24 12:25:48 +0800

    Add home page html

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,10 +1,49 @@
 import React from 'react'
+import '../css/style.css'
 
 class HomePage extends React.Component {
   render () {
     return (
       <div>
-        <p> HomePage Test </p>
+        <section id='banner'>
+          <h2>歡迎來到surfing penguin</h2>
+          <ul class='actions'>
+            <li>
+              <a href='#' class='button big'>建立問卷</a>
+            </li>
+          </ul>
+        </section>
+        <section id='one' class='wrapper style1 special'>
+          <div class='container'>
+            <header class='major'>
+              <h2>Consectetur adipisicing elit</h2>
+              <p>Lorem ipsum dolor sit amet, delectus consequatur, similique quia!</p>
+            </header>
+            <div class='row 150%'>
+              <div class='4u 12u$(medium)'>
+                <section class='box'>
+                  <i class='icon big rounded color1 fa-cloud' />
+                  <h3>Lorem ipsum dolor</h3>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim quam consectetur quibusdam magni minus aut modi aliquid.</p>
+                </section>
+              </div>
+              <div class='4u 12u$(medium)'>
+                <section class='box'>
+                  <i class='icon big rounded color9 fa-desktop' />
+                  <h3>Consectetur adipisicing</h3>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium ullam consequatur repellat debitis maxime.</p>
+                </section>
+              </div>
+              <div class='4u$ 12u$(medium)'>
+                <section class='box'>
+                  <i class='icon big rounded color6 fa-rocket' />
+                  <h3>Adipisicing elit totam</h3>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque eaque eveniet, nesciunt molestias. Ipsam, voluptate vero.</p>
+                </section>
+              </div>
+            </div>
+          </div>
+        </section>
         {this.props.children}
       </div>
     )
