1c7236e182dfa1b2608725bf68ccce680c35b7cc
b05902128@ntu.edu.tw <b05902128@ntu.edu.tw>
2018-08-28 12:27:17 +0800

    Rename class to className

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -7,36 +7,36 @@ class HomePage extends React.Component {
       <div>
         <section id='banner'>
           <h2>歡迎來到surfing penguin</h2>
-          <ul class='actions'>
+          <ul className='actions'>
             <li>
-              <a href='#' class='button big'>建立問卷</a>
+              <a href='#' className='button big'>建立問卷</a>
             </li>
           </ul>
         </section>
-        <section id='one' class='wrapper style1 special'>
-          <div class='container'>
-            <header class='major'>
+        <section id='one' className='wrapper style1 special'>
+          <div className='container'>
+            <header className='major'>
               <h2>Consectetur adipisicing elit</h2>
               <p>Lorem ipsum dolor sit amet, delectus consequatur, similique quia!</p>
             </header>
-            <div class='row 150%'>
-              <div class='4u 12u$(medium)'>
-                <section class='box'>
-                  <i class='icon big rounded color1 fa-cloud' />
+            <div className='row 150%'>
+              <div className='4u 12u$(medium)'>
+                <section className='box'>
+                  <i className='icon big rounded color1 fa-cloud' />
                   <h3>Lorem ipsum dolor</h3>
                   <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim quam consectetur quibusdam magni minus aut modi aliquid.</p>
                 </section>
               </div>
-              <div class='4u 12u$(medium)'>
-                <section class='box'>
-                  <i class='icon big rounded color9 fa-desktop' />
+              <div className='4u 12u$(medium)'>
+                <section className='box'>
+                  <i className='icon big rounded color9 fa-desktop' />
                   <h3>Consectetur adipisicing</h3>
                   <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium ullam consequatur repellat debitis maxime.</p>
                 </section>
               </div>
-              <div class='4u$ 12u$(medium)'>
-                <section class='box'>
-                  <i class='icon big rounded color6 fa-rocket' />
+              <div className='4u$ 12u$(medium)'>
+                <section className='box'>
+                  <i className='icon big rounded color6 fa-rocket' />
                   <h3>Adipisicing elit totam</h3>
                   <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque eaque eveniet, nesciunt molestias. Ipsam, voluptate vero.</p>
                 </section>
