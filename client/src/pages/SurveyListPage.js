28ba15e04cca09abd1a78a205b299b87c4b96674
Allen <b03901014@ntu.edu.tw>
2018-09-07 12:06:28 +0800

    Fix css style

--- client/src/pages/SurveyListPage.js
+++ client/src/pages/SurveyListPage.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react'
 import '../css/search.css'
+// import '../css/bootstrap.css'
+import '../css/font-awesome/css/font-awesome.min.css'
 
 class SurveyList extends Component {
   render () {
     return (
-      <div className='Surveys'>
-        <p> Surveys Test</p>
-        {this.props.children}
+      <div className='SurveyList'>
         <div id='sidebar'>
           <ul>
             <li><a href='#'>我的問卷</a> </li>
@@ -17,10 +17,10 @@ class SurveyList extends Component {
         </div>
         <div id='search'>
 
-          <form classname='form-inline'>
+          <form className='form-inline'>
 
-            <input classname='form-control mr-sm-2' type='search' placeholder='關鍵字' aria-label='Search' />
-            <i classname='fa fa-search fa-2x' aria-hidden='true' />
+            <input className='form-control mr-sm-2' type='search' placeholder='關鍵字' aria-label='Search' />
+            <i className='fa fa-search fa-2x' aria-hidden='true' />
           </form>
         </div>
 
@@ -30,50 +30,50 @@ class SurveyList extends Component {
             <li><a href='#'>熱門問卷</a> </li>
             <li><a href='#'>已截止</a> </li>
             <li>
-              <button type='button' classname='create_btn'><i classname='fa fa-pencil' aria-hidden='true'> 建問卷</i></button>
+              <button type='button' className='create_btn'><i className='fa fa-pencil' aria-hidden='true'> 建問卷</i></button>
             </li>
           </ul>
         </div>
         <div id='content'>
-          <div classname='list-group'>
+          <div className='list-group'>
 
-            <a href='#' classname='list-group-item list-group-item-action flex-column align-items-start'>
-              <div classname='d-flex w-100 justify-content-between'>
-                <h5 classname='mb-1'>標題</h5>
+            <a href='#' className='list-group-item list-group-item-action flex-column align-items-start'>
+              <div className='d-flex w-100 justify-content-between'>
+                <h5 className='mb-1'>標題</h5>
                 <small>已截止</small>
               </div>
-              <p classname='mb-1'>Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
-              <i classname='fa fa-bookmark' aria-hidden='true'> 收藏</i>
-              <i classname='fa fa-pencil' aria-hidden='true'> 60</i>
+              <p className='mb-1'>Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
+              <i className='fa fa-bookmark' aria-hidden='true'> 收藏</i>
+              <i className='fa fa-pencil' aria-hidden='true'> 60</i>
             </a>
 
-            <a href='#' classname='list-group-item list-group-item-action flex-column align-items-start'>
-              <div classname='d-flex w-100 justify-content-between'>
-                <h5 classname='mb-1'>標題</h5>
+            <a href='#' className='list-group-item list-group-item-action flex-column align-items-start'>
+              <div className='d-flex w-100 justify-content-between'>
+                <h5 className='mb-1'>標題</h5>
                 <small>進行中</small>
               </div>
-              <p classname='mb-1'>Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
-              <i classname='fa fa-bookmark' aria-hidden='true'>收藏</i>
-              <i classname='fa fa-pencil' aria-hidden='true'> 50</i>
+              <p className='mb-1'>Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
+              <i className='fa fa-bookmark' aria-hidden='true'>收藏</i>
+              <i className='fa fa-pencil' aria-hidden='true'> 50</i>
             </a>
 
-            <a href='#' classname='list-group-item list-group-item-action flex-column align-items-start'>
-              <div classname='d-flex w-100 justify-content-between'>
-                <h5 classname='mb-1'>標題</h5>
+            <a href='#' className='list-group-item list-group-item-action flex-column align-items-start'>
+              <div className='d-flex w-100 justify-content-between'>
+                <h5 className='mb-1'>標題</h5>
                 <small>已截止</small>
               </div>
-              <p classname='mb-1'>Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
-              <i classname='fa fa-bookmark' aria-hidden='true'>收藏</i>
-              <i classname='fa fa-pencil' aria-hidden='true'> 100</i>
+              <p className='mb-1'>Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
+              <i className='fa fa-bookmark' aria-hidden='true'>收藏</i>
+              <i className='fa fa-pencil' aria-hidden='true'> 100</i>
             </a>
-            <a href='#' classname='list-group-item list-group-item-action flex-column align-items-start'>
-              <div classname='d-flex w-100 justify-content-between'>
-                <h5 classname='mb-1'>標題</h5>
+            <a href='#' className='list-group-item list-group-item-action flex-column align-items-start'>
+              <div className='d-flex w-100 justify-content-between'>
+                <h5 className='mb-1'>標題</h5>
                 <small>已截止</small>
               </div>
-              <p classname='mb-1'>Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
-              <i classname='fa fa-bookmark' aria-hidden='true'>收藏</i>
-              <i classname='fa fa-pencil' aria-hidden='true'> 20</i>
+              <p className='mb-1'>Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
+              <i className='fa fa-bookmark' aria-hidden='true'>收藏</i>
+              <i className='fa fa-pencil' aria-hidden='true'> 20</i>
             </a>
           </div>
         </div>
