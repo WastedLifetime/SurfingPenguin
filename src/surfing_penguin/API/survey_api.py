4f09e3c25f0ff2baf0b651be1b1aa973eba6fe7a
zfrank7777 <b05902022@ntu.edu.tw>
2018-09-07 10:31:36 +0800

    Modify API models and create_survey api

--- src/surfing_penguin/API/survey_api.py
+++ src/surfing_penguin/API/survey_api.py
@@ -29,6 +29,10 @@ api_question = api.model("question_model", {
 
 api_get_survey = api.model("get_survey_model", {
         'surveyname': fields.String(description="Survey name"),
+        'survey_description': fields.String(
+            description="Detailed description of the survey"),
+        'prize_description': fields.String(
+            description="Description of prize of the survey"),
         'questions': fields.List(
             fields.Nested(api_question),
             description="All questions in the survey"
@@ -43,6 +47,10 @@ api_return_survey = api.model("return_survey_model", {
         'question_num': fields.Integer(
             description="Number of questions in the survey"),
         'surveyname': fields.String(description="Survey name"),
+        'survey_description': fields.String(
+            description="Detailed description of the survey"),
+        'prize_description': fields.String(
+            description="Description of prize of the survey"),
         'questions': fields.List(
             fields.Nested(api_question),
             description="All questions in the survey"
