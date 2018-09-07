a5ea548787c1cd2822878a2787d7d3028744c6cf
zfrank7777 <b05902022@ntu.edu.tw>
2018-09-07 10:26:24 +0800

    Modify data format of new_survey in db interface

--- src/surfing_penguin/db_interface/survey_functions.py
+++ src/surfing_penguin/db_interface/survey_functions.py
@@ -2,12 +2,12 @@ from src.surfing_penguin.extensions import session
 from src.surfing_penguin.models import Survey, Question, AnswerList, Answer
 
 
-def new_survey(name, questions):
-    survey = Survey(name)
+def new_survey(data):
+    survey = Survey(data)
     session.add(survey)
     session.commit()
-    for i in range(len(questions)):
-        new_question(survey, questions[i])
+    for i in range(len(data['questions'])):
+        new_question(survey, data['questions'][i])
     return survey
 
 
--- src/surfing_penguin/models.py
+++ src/surfing_penguin/models.py
@@ -56,8 +56,11 @@ class Survey(Base):
     questions = relationship("Question")
     answerlists = relationship("AnswerList")
 
-    def __init__(self, name):
-        self.surveyname = name
+    def __init__(self, data):
+        self.surveyname = data['name']
+        self.author = data['author']
+        self.survey_description = data['survey_description']
+        self.prize_description = data['prize_description']
         self.question_num = 0
         self.answerlist_num = 0
 
