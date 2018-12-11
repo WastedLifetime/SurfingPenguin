b9f2ccdf438bb55a234b43df5888f63f8a6dd257
Allen <b03901014@ntu.edu.tw>
2018-12-11 19:31:58 +0800

    Fix user name display

--- client/src/containers/SideNavBar.js
+++ client/src/containers/SideNavBar.js
@@ -10,13 +10,24 @@ import './SideNavBar.css'
 class SideNavBar extends Component {
   render () {
     let { currentUser } = this.props
+    currentUser = currentUser.slice(7)
     // if (currentUser) { indexPathname = '#/surveys' }
     return (
       <Nav pullRight bsStyle='SideNavBar'>
 
         <NavDropdown title={<span class='glyphicon glyphicon-user' aria-hidden='true'> {currentUser} </span>}>
-          <MenuItem href='#/logout'>Logout </MenuItem>
-          <MenuItem href='#/' active>Donate</MenuItem>
+          <MenuItem href='#/user_profile'>{currentUser}</MenuItem>
+          <MenuItem divider />
+          <MenuItem href='#/create_survey'>建立問卷</MenuItem>
+          <MenuItem divider />
+          <MenuItem href='#/my_survey' active>我的問卷</MenuItem>
+          <MenuItem href='#/shared_survey' active>共用問卷</MenuItem>
+          <MenuItem href='#/collected_survey' active>收藏問卷</MenuItem>
+          <MenuItem divider />
+          <MenuItem href='#/setting' active>設定</MenuItem>
+          <MenuItem href='#/support' active>支援</MenuItem>
+          <MenuItem href='#/contact_us' active>聯絡我們</MenuItem>
+          <MenuItem href='#/privacy' active>隱私權政策</MenuItem>
           <MenuItem divider />
           <MenuItem href='#/logout'>登出 </MenuItem>
           <MenuItem href='#/' active>Donate</MenuItem>
