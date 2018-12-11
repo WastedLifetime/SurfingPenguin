2b0dbfb15c4fd00019bb56e89d09682e9a2cc5fd
Allen <b03901014@ntu.edu.tw>
2018-12-11 19:59:36 +0800

    Remove redundant styles in sideNavBar

--- client/src/containers/SideNavBar.js
+++ client/src/containers/SideNavBar.js
@@ -4,7 +4,7 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { withRouter } from 'react-router'
 import {Nav, NavDropdown, MenuItem} from 'react-bootstrap'
-import '../css/style.css'
+// import '../css/style.css'
 import './SideNavBar.css'
 
 class SideNavBar extends Component {
@@ -20,14 +20,14 @@ class SideNavBar extends Component {
           <MenuItem divider />
           <MenuItem href='#/create_survey'>建立問卷</MenuItem>
           <MenuItem divider />
-          <MenuItem href='#/my_survey' active>我的問卷</MenuItem>
-          <MenuItem href='#/shared_survey' active>共用問卷</MenuItem>
-          <MenuItem href='#/collected_survey' active>收藏問卷</MenuItem>
+          <MenuItem href='#/my_survey'>我的問卷</MenuItem>
+          <MenuItem href='#/shared_survey'>共用問卷</MenuItem>
+          <MenuItem href='#/collected_survey'>收藏問卷</MenuItem>
           <MenuItem divider />
-          <MenuItem href='#/setting' active>設定</MenuItem>
-          <MenuItem href='#/support' active>支援</MenuItem>
-          <MenuItem href='#/contact_us' active>聯絡我們</MenuItem>
-          <MenuItem href='#/privacy' active>隱私權政策</MenuItem>
+          <MenuItem href='#/setting'>設定</MenuItem>
+          <MenuItem href='#/support'>支援</MenuItem>
+          <MenuItem href='#/contact_us'>聯絡我們</MenuItem>
+          <MenuItem href='#/privacy'>隱私權政策</MenuItem>
           <MenuItem divider />
           <MenuItem href='#/logout'>登出 </MenuItem>
           <MenuItem href='#/' active>Donate</MenuItem>
